157192b14aee333c6ac4a8b665943e0e38a59e8b
= <=>
2023-01-04 20:17:03 +0100

    Forgot to fix the test

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -4,8 +4,7 @@ import "testing"
 
 func TestSayHello(t *testing.T) {
 	greeting := SayHello()
-	// this should fail
-	if greeting == "hello" {
+	if greeting != "hello" {
 		t.Fatal("Expected greeting to be hello")
 	}
 }
